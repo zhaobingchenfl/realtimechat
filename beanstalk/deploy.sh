94acf71ad86924ccac884018b039dfc1e8b9faa6
zhaobingchenfl <zhaobingchen@yahoo.com>
2019-04-28 21:13:55 +0000

    Test

--- beanstalk/deploy.sh
+++ beanstalk/deploy.sh
@@ -3,8 +3,13 @@
 mkdir -p beanstalk/app
 cp beanstalk/Dockerrun.aws.json beanstalk/app/
 cd beanstalk/app
+
+set -x
 export PATH=$PATH:/home/ec2-user/.local/bin
 
+echo $PATH
+echo $PWD
+
 eb init -p docker zchen-eb-docker
-eb deploy ZchenEbDocker-env -region us-east-2
+eb deploy ZchenEbDocker-env --region us-east-2
 
