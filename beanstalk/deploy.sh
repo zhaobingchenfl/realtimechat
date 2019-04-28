1a90e3e101fc6a3d231c30ac8ac8ea83701dafde
zhaobingchenfl <zhaobingchen@yahoo.com>
2019-04-28 20:56:46 +0000

    Add path

--- beanstalk/deploy.sh
+++ beanstalk/deploy.sh
@@ -3,6 +3,7 @@
 mkdir -p beanstalk/app
 cp beanstalk/Dockerrun.aws.json beanstalk/app/
 cd beanstalk/app
+export PATH=$PATH:/home/ec2-user/.local/bin
 
 eb init -p docker zchen-eb-docker
 eb deploy ZchenEbDocker-env -region us-east-2
